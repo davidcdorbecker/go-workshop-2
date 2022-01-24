39c226dc102098341b39e9f93e18262c1f816907
David Jose Castillo Dorbecker <ext-d.castillo@rappi.com>
2022-01-24 07:54:27 -0600

    initial state

--- Maps functions and pointers/section-1-maps/main.go
+++ Maps functions and pointers/section-1-maps/main.go
@@ -14,26 +14,28 @@ func main() {
 	}
 
 	fmt.Println(mexicanEquivalence)
-
-	mexicanEquivalence["YEN"] = 5.5211869897629
-
-	fmt.Println(mexicanEquivalence)
-	fmt.Println(mexicanEquivalence["UNDEFINED EQUIVALENCE"])
+	//
+	//mexicanEquivalence["YEN"] = 5.5211869897629
+	//
+	//fmt.Println(mexicanEquivalence)
+	//fmt.Println(mexicanEquivalence["UNDEFINED EQUIVALENCE"])
 
 	//mexicanEquivalence["YEN"] = 5.5211869897629
 	//delete(mexicanEquivalence, "PESO CHILENO")
 	//fmt.Println(mexicanEquivalence)
 	//
-	eq := mexicanEquivalence["?"]
-	fmt.Println(eq)
+	//eq := mexicanEquivalence["?"]
+	//fmt.Println(eq)
 
 	//copy := mexicanEquivalence
 	//copy["DOLAR"] = 0.06
 	//fmt.Println(copy, mexicanEquivalence)
 
-	mapOfMaps := map[string]map[string]int {
-		"a": {"b": 1},
-	}
+	//iterating over map
 
-	fmt.Println(mapOfMaps)
+	//mapOfMaps := map[string]map[string]int {
+	//	"a": {"b": 1},
+	//}
+	//
+	//fmt.Println(mapOfMaps)
 }
